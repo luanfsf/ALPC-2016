1cc0098ced83f3da070ae609f5b58351f6c0b1d8
Luan Ferreira <luanfsf@gmail.com>
2016-09-14 15:57:07 -0300

    Filled the test with basic js.

---
+++ javascript/test.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Arquivo de teste.</title>
+<meta htp-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="text/javascript">
+function hello() {
+  
+  var mensagem = "Oi!, :-)"
+  document.open();
+  document.write(mensagem);
+  document.close();
+  
+}
+
+</script>
+</head>
+<body onload="hello()">
+<p>:-)</p>
+<body>
+</html>
