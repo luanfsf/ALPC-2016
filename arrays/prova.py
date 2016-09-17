df3761cfb0a868bbfbf9acc028d2c32a0b11c5a7
luanfsf <luanfsf@gmail.com>
2016-09-17 14:39:04 -0400

    :)

---
+++ arrays/prova.py
@@ -0,0 +1,34 @@
+clientes = [0] * 30
+ceps = [0] * 30
+valvendas [0] * 30
+
+
+contvendas = 0
+
+combo1 = 0
+combo2= 0
+combo3 = 0
+
+
+while contvendas < 30:
+  print
+  print
+  print
+  print
+  
+  opcao = int(input())
+  
+  if opcao == 1:
+    """realizar pedido
+    CEP
+    combo (combo1, combo2, combo3)
+    quntidade do combo"""
+    
+  if opcao == 2:
+    # CEP do maior e do menor pedido
+  if opcao == 3:
+    """exibir quantos pedidos foram feitos
+    de cada um dos 3 combos, se a quantidade
+    for > que 1."""
+  if opcao == 4:
+    #total de vendas
