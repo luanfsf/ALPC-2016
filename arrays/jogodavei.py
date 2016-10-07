93b31c752cc0aef85937a0c0491151112aea8760
luanfsf <luanfsf@gmail.com>
2016-10-07 15:32:06 -0300

    jogo da velha

--- arrays/jogodavei.py
+++ arrays/jogodavei.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python
+
+velha = [[" "] * 3 for i in range (3) ]
 print "Jogo da velha!"
 print "X comeca",
-velha = [[" "] * 3 for i in range (3) ]
+
 linha = 1
 coluna = 1
 jogador = 1
 fim = 1
+
+
 while (fim != 0):
   print "digite o local de acordo com a tabela"
   print "    1  2  3"
   print " 1  _  _  _"
   print " 2  _  _  _"
   print " 3  _  _  _"
-  print "Jogador:",jogador
-  print "Linha:"
-  linha = int(input())
-  print "Coluna:"
-  coluna = int(input())
-  
-  if jogador == 2 and velha[linha-1][coluna-1] != "X" and velha[linha-1][coluna-1] != "O":
-    velha[linha-1][coluna-1] = "O"
-    jogador -= 1
-    
-  if jogador == 1 and velha[linha-1][coluna-1] != "X" and velha[linha-1][coluna-1] != "O":
-    velha[linha-1][coluna-1] = "X"
-    jogador += 1
-  
+
+
+
+  ok = 1
+  while (ok =! 0)
+    print "Jogador:",jogador
+    print "Linha:"
+    linha = int(input())
+    print "Coluna:"
+    coluna = int(input())
+    if jogador == 2 and velha[linha-1][coluna-1] != "X" and velha[linha-1][coluna-1] != "O":
+        velha[linha-1][coluna-1] = "O"
+        jogador -= 1
+        ok = 0
+    if jogador == 1 and velha[linha-1][coluna-1] != "X" and velha[linha-1][coluna-1] != "O":
+       velha[linha-1][coluna-1] = "X"
+       jogador += 1
+       ok =0
+
   for i in range (3):
     print "\n-------------"
     for j in range (3):
       print "|", velha[i][j],
-      
+
     print "|",
   print "\n-------------"
 
