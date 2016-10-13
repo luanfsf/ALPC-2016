cec71046b024d163f25dec75e467b725438ce3ed
luanfsf <luanfsf@gmail.com>
2016-10-12 23:28:07 -0300

    fibonacci sem recalcular muitos valores

---
+++ algorithms/fibonacci-memoization.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+numero = 0
+
+
+while ( numero < 1 or numero > 1000):
+  print "digite um numero de 1 a 1000"
+  numero = int(input())
+
+valores = {}
+  
+def fibonacci(n):
+  if n in valores[n]:
+  return valores[n]
+  
+  if n == 1:
+    valor 1
+  elif n == 2:
+    valor 1
+  elif n > 2:
+    value = fibonacci(n-1) + fibonacci(n-2)
+  
+for i in range(1, numero +1):
+  print i, ":", fibonacci(i) 
