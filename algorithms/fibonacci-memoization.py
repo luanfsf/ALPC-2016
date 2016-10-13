1210ffe0a163d609155401a827b9cffcc8182913
luanfsf <luanfsf@gmail.com>
2016-10-12 22:50:17 -0400

    fibonacci com recursao mas com memoization

--- algorithms/fibonacci-memoization.py
+++ algorithms/fibonacci-memoization.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 numero = 0
 
-
 while ( numero < 1 or numero > 1000):
   print "digite um numero de 1 a 1000"
   numero = int(input())
 
-valores = {}
-  
+valores = {}  
 def fibonacci(n):
-  if n in valores[n]:
-  return valores[n]
+  if n in valores:
+    return valores[n]
   
   if n == 1:
-    valor 1
+    valor  = 1
   elif n == 2:
-    valor 1
+    valor = 1
   elif n > 2:
-    value = fibonacci(n-1) + fibonacci(n-2)
+    valor = fibonacci(n-1) + fibonacci(n-2)
   
-for i in range(1, numero +1):
+  valores[n]= valor
+  return valor
+  
+for i in range(1, numero + 1 ):
   print i, ":", fibonacci(i) 
