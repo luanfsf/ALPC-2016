cabafcc5aa3a2f00a1887c98496ff4d225d66da3
Luan Fernaando de Souza Ferreira <luanfsf@gmail.com>
2016-10-25 21:26:53 +0000

    adicainado duas matrizes e movido um jogo

---
+++ arrays/2Darrays/matrix+-matrix.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# python 2.7
+import random
+
+# gerar duas matrizes NxN e perguntar
+# se deseja somar ou diminuir a primeira pela segunda
+
+dimensao = 0
+
+while dimensao < 1 or dimensao > 10 :
+    print "Digite a dimensão das matrizes, de 1 a 10 :",
+    dimensao = int(input())
+
+matrix1 = [[random.randrange(1, 9999, 1) for i in range(dimensao)] for j in range(dimensao)]
+matrix2 = [[random.randrange(1, 9999, 1) for i in range(dimensao)] for j in range(dimensao)]
+resultado = [[0] * dimensao for i in range(dimensao)]
+
+for linha in range(dimensao): # Exibe a grade da matrix
+    for coluna in range(dimensao):
+        print "|%5d" %matrix1[linha][coluna],
+    print "| >>>>",
+    for coluna in range(dimensao):
+        print "|%5d" %matrix2[linha][coluna],
+    print "|",
+    print ""
+
+opcao = 0
+while opcao <1 or opcao > 2:
+    print "Se deseja somar as matrizes digite 1\nSe deseja diminuir digite 2"
+    opcao = int(input())
+
+if opcao == 1:
+    for i in range(dimensao):
+        for j in range(dimensao):
+            resultado[i][j] = matrix1[i][j] + matrix2[i][j]
+
+print "Resultado"
+if opcao == 2:
+    for i in range(dimensao):
+        for j in range(dimensao):
+            resultado[i][j] = matrix1[i][j] - matrix2[i][j]
+            
+print "Resultado"
+for linha in range(dimensao): # Exibe a grade da matrix
+    for coluna in range(dimensao):
+        print "| %5d" %resultado[linha][coluna],
+    print "|",
+    print ""
